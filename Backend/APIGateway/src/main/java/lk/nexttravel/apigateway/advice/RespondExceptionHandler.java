cf21c2e7c5a456c962df98893850e498ce378f67
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-25 18:58:58 +0530

    respondException handle class implementation

--- Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/advice/RespondExceptionHandler.java
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/advice/RespondExceptionHandler.java
@@ -1,9 +1,33 @@
 package lk.nexttravel.apigateway.advice;
 
+import lk.nexttravel.apigateway.advice.util.InvalidPasswordException;
+import lk.nexttravel.apigateway.dto.RespondDTO;
+import lk.nexttravel.apigateway.util.ResCodes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 25/10/2023
  * Time    : 13:39
  */
+@ControllerAdvice
 public class RespondExceptionHandler {
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    protected ResponseEntity<RespondDTO> exception (InvalidPasswordException invalidPasswordException){
+        return new ResponseEntity<RespondDTO>(
+                (RespondDTO.builder()
+                        .res_code(ResCodes.Response_NOT_AUTHORISED)
+                        .res_mg(invalidPasswordException.getMessage())
+                        .token(null)
+                        .data(null)
+                        .build()
+                ),
+                HttpStatus.UNAUTHORIZED
+
+        );
+    }
 }
---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/util/ResCodes.java
@@ -0,0 +1,24 @@
+package lk.nexttravel.apigateway.util;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 25/10/2023
+ * Time    : 18:51
+ */
+public class ResCodes {
+
+    public static final String Response_SUCCESS                   = "00";
+    public static final String Response_NO_DATA_FOUND             = "01";
+    public static final String Response_NOT_AUTHORISED            = "02";
+    public static final String Response_TOKEN_EXPIRED             = "03";
+    public static final String Response_TOKEN_INVALID             = "04";
+    public static final String Response_ERROR                     = "05";
+    public static final String Response_DUPLICATED                = "06";
+    public static final String Response_FAIL                      = "10";
+    public static final String Response_SERVERSIDE_INTERNAL_FAIL  = "11";
+    public static final String Response_DATA_INVALID              = "12";
+    public static final String Response_AUTHORISED                = "13";
+    public static final String Response_DATA_SAVED                = "14";
+    public static final String Response_INTERNAL_SERVER_FAIL      = "15";
+    public static final String Response_New_Tokens_Generated      = "C100";
+}
