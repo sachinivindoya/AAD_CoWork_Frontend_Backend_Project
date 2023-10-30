b7fed9e9ce6582a8febbcbc8079cb16d47cf10e2
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-31 00:42:56 +0530

    user micro service

---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package lk.nexttravel.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 31/10/2023
+ * Time    : 00:28
+ */
+@Configuration
+
+public class SecurityConfig {
+}
--- Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/service/serviceImpl/UserServiceImpl.java
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/service/serviceImpl/UserServiceImpl.java
@@ -2,10 +2,20 @@ package lk.nexttravel.apigateway.service.serviceImpl;
 
 import lk.nexttravel.apigateway.dto.RespondDTO;
 import lk.nexttravel.apigateway.dto.auth.UserSignupDTO;
+import lk.nexttravel.apigateway.entity.User;
+import lk.nexttravel.apigateway.repostory.UserRepository;
+import lk.nexttravel.apigateway.service.SequenceGeneratorService;
 import lk.nexttravel.apigateway.service.UserService;
+import lk.nexttravel.apigateway.util.ResCodes;
+import lk.nexttravel.apigateway.util.RoleTypes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
+import java.util.Optional;
 
 /**
  * @author : R.M.Sachini Vinodya
@@ -14,6 +24,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
     @Override
     public ResponseEntity<RespondDTO> IsUsernameAlreadyHave(String username) {
         return null;
@@ -21,15 +40,32 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseEntity<RespondDTO> saveNewUser(UserSignupDTO userSignupDTO) {
-        RespondDTO respondDTO = new RespondDTO();
-        System.out.println("method invoked");
-        return new ResponseEntity<>(
-                respondDTO
-                ,
-                HttpStatus.ACCEPTED
+        System.out.println("saved");
+        String id = "U00"+sequenceGeneratorService.generateSequence(User.SEQUENCE_NAME);
+//        String password = passwordEncoder.encode(userSignupDTO.getSignup_password());
+        String encodedImage = Base64.getEncoder().encodeToString(userSignupDTO.getSignupprofile_image());
+        Optional<User> savedUser;
+        savedUser = Optional.of(
+                userRepository.save(
+                        User.builder()
+                                .id(id)
+                                .name(userSignupDTO.getSignup_name())
+                                .email(userSignupDTO.getSignup_email())
+                                .password(userSignupDTO.getSignup_password())
+                                .roleTypes(RoleTypes.CLIENT)
+
+                                .build()
+                )
 
         );
-
+              return new ResponseEntity<RespondDTO> (
+                     RespondDTO.builder()
+                      .res_code(ResCodes.Response_DATA_SAVED)
+//                      .token(frontendTokenDTO)
+                      .data(savedUser.get().getRoleTypes())
+                      .build()
+                        ,
+                      HttpStatus.CREATED);
 
     }
 
