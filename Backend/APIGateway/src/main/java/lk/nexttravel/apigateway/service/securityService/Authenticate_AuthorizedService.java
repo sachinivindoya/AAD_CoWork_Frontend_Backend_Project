e476febbcc31fc0c5679b6b3ec89688406ee7b0a
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-31 01:03:10 +0530

    user micro service

---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/service/securityService/Authenticate_AuthorizedService.java
@@ -0,0 +1,10 @@
+package lk.nexttravel.apigateway.service.securityService;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 31/10/2023
+ * Time    : 00:59
+ */
+public interface Authenticate_AuthorizedService {
+
+}
---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/service/securityService/util/RefreshTokenServiceFrontend.java
@@ -0,0 +1,9 @@
+package lk.nexttravel.apigateway.service.securityService.util;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 31/10/2023
+ * Time    : 01:03
+ */
+public class RefreshTokenServiceFrontend {
+}
