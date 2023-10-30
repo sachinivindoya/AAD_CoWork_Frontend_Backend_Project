3a7aeb6d8e38d35df2e5182cac9d72caf3a9d181
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-30 17:00:54 +0530

    user service

---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/service/serviceImpl/SequenceGeneratorServiceImpl.java
@@ -0,0 +1,38 @@
+package lk.nexttravel.apigateway.service.serviceImpl;
+
+import lk.nexttravel.apigateway.entity.util.DBSequensee;
+import lk.nexttravel.apigateway.service.SequenceGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 30/10/2023
+ * Time    : 16:51
+ */
+@Service
+public class SequenceGeneratorServiceImpl implements SequenceGeneratorService {
+    @Override
+    public long generateSequence(String seqName) {
+        return 0;
+    }
+//    @Autowired
+//    private MongoOperations mongoOperations;
+//
+//    @Autowired
+//    public SequenceGeneratorServiceImpl(MongoOperations mongoOperations) {
+//
+//        this.mongoOperations = mongoOperations;
+//    }
+//
+//    @Override
+//    public long generateSequence(String seqName) {
+//        DBSequensee counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
+//                new Update().inc("seq",1), options().returnNew(true).upsert(true),
+//                DBSequensee.class);
+//        return !Objects.isNull(counter) ? counter.getSeq() : 1;
+//    }
+}
---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/util/ReqRepURLs.java
@@ -0,0 +1,13 @@
+package lk.nexttravel.apigateway.util;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 30/10/2023
+ * Time    : 16:57
+ */
+public class ReqRepURLs {
+    //Client Service - API Gateway Services
+    public static final String User_Service_New_Client_Save_Signup              = "http://localhost:1020/api/client/save_new_client";
+
+    public static final String User_Service_New_Admin_Save_Signup              = "http://localhost:1020/api/admin/save_new_admin";
+}
