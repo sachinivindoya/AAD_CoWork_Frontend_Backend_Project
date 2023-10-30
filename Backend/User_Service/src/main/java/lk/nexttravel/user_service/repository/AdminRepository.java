1d3ecdff955c9165939577e5eefcad8ee3506122
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-30 18:02:24 +0530

    user service

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/repository/AdminRepository.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/repository/AdminRepository.java
@@ -1,9 +1,15 @@
 package lk.nexttravel.user_service.repository;
 
+import lk.nexttravel.user_service.entity.Admin;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 30/10/2023
  * Time    : 18:00
  */
-public interface AdminRepository {
+public interface AdminRepository extends MongoRepository<Admin,Long> {
+    Optional<Admin> findAdminById(String id);
 }
