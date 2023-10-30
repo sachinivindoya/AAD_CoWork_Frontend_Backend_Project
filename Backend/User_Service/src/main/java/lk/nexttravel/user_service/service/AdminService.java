d52fdff0afa08868e181f3554b89bbcdae818def
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-30 18:06:27 +0530

    user service

---
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/service/AdminService.java
@@ -0,0 +1,9 @@
+package lk.nexttravel.user_service.service;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 30/10/2023
+ * Time    : 18:05
+ */
+public interface AdminService {
+}
---
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/service/ClientService.java
@@ -0,0 +1,9 @@
+package lk.nexttravel.user_service.service;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 30/10/2023
+ * Time    : 18:06
+ */
+public interface ClientService {
+}
