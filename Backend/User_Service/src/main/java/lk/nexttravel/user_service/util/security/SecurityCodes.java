6fa148666064c934be42d67bfd2eb4a982ea119a
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-30 17:18:41 +0530

    user service

---
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/util/security/SecurityCodes.java
@@ -0,0 +1,10 @@
+package lk.nexttravel.user_service.util.security;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 30/10/2023
+ * Time    : 17:18
+ */
+public class SecurityCodes {
+    //API Gateway Token key - For Backend connection with api gateway
+}
