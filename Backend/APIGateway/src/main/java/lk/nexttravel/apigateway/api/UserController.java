587e3f8832c8147ea8680415cef8a6bfb8854c6a
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-30 16:19:04 +0530

    user mcroservce

--- Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/api/UserController.java
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/api/UserController.java
@@ -60,59 +60,59 @@ public class UserController {
 //    }
 //
 //
-//    // save new user
-//
-//    @PostMapping(value = "/signup-user", consumes = MediaType.MULTIPART_FORM_DATA_VALUE) // fetch data from front end data type
-//    public Mono<ResponseEntity<RespondDTO>> saveNewUser (
-//            @RequestPart("signup_name") @NonNull String name,
-//            @RequestPart("signup_address") @NonNull String address,
-//            @RequestPart("signup_nic_or_passport") @NonNull String nic_Passport,
-//            @RequestPart("signup_email") @NonNull String email,
-//            @RequestPart("signup_username") @NonNull String username,
-//            @RequestPart("signup_password") @NonNull String password,
-//            @RequestPart("signupprofile_image") byte[] image){
-//
-//        if(name.matches("^[a-zA-Z0-9_.-]{4,30}$")){
-//            if(address.matches("^\\S+\\s*[a-zA-Z0-9,.-]+\\S{0,48}$")){
-//                if(nic_Passport.matches("^[a-zA-Z0-9_-]+\\S{8,11}$")){
-//                    if(email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")){
-//                        if(username.matches("^[a-zA-Z0-9_.-]{5,30}$")){
-//                            if (password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$")){
-//                                if(image != null){
-//
-//                                    return Mono.just(
-//                                            userService.saveNewUser(
-//                                                    UserSignupDTO.builder()
-//                                                            .signup_name(name)
-//                                                            .signup_address(address)
-//                                                            .signup_nic_or_passport(nic_Passport)
-//                                                            .signup_email(email)
-//                                                            .signup_username(username)
-//                                                            .signup_password(password)
-//                                                            .signupprofile_image(image)
-//                                                            .build()
-//                                            )
-//                                    );
-//                                } else {
-//                                    throw new InvalidDataEntryException("invalid image");
-//                                }
-//                            } else {
-//                                throw new InvalidDataEntryException("invalid password");
-//                            }
-//                        } else {
-//                            throw new InvalidDataEntryException("invalid username");
-//                        }
-//                    } else {
-//                        throw new InvalidDataEntryException("invalid email");
-//                    }
-//                } else {
-//                    throw new InvalidDataEntryException("invalid nic or passport number");
-//                }
-//            } else {
-//                throw new InvalidDataEntryException("invalid address");
-//            }
-//        } else {
-//            throw new InvalidDataEntryException("invalid name");
-//        }
-//    }
+    // save new user
+
+    @PostMapping(value = "/signup-user", consumes = MediaType.MULTIPART_FORM_DATA_VALUE) // fetch data from front end data type
+    public Mono<ResponseEntity<RespondDTO>> saveNewUser (
+            @RequestPart("signup_name") @NonNull String name,
+            @RequestPart("signup_address") @NonNull String address,
+            @RequestPart("signup_nic_or_passport") @NonNull String nic_Passport,
+            @RequestPart("signup_email") @NonNull String email,
+            @RequestPart("signup_username") @NonNull String username,
+            @RequestPart("signup_password") @NonNull String password,
+            @RequestPart("signupprofile_image") byte[] image){
+
+        if(name.matches("^[a-zA-Z0-9_.-]{4,30}$")){
+            if(address.matches("^\\S+\\s*[a-zA-Z0-9,.-]+\\S{0,48}$")){
+                if(nic_Passport.matches("^[a-zA-Z0-9_-]+\\S{8,11}$")){
+                    if(email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")){
+                        if(username.matches("^[a-zA-Z0-9_.-]{5,30}$")){
+                            if (password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$")){
+                                if(image != null){
+
+                                    return Mono.just(
+                                            userService.saveNewUser(
+                                                    UserSignupDTO.builder()
+                                                            .signup_name(name)
+                                                            .signup_address(address)
+                                                            .signup_nic_or_passport(nic_Passport)
+                                                            .signup_email(email)
+                                                            .signup_username(username)
+                                                            .signup_password(password)
+                                                            .signupprofile_image(image)
+                                                            .build()
+                                            )
+                                    );
+                                } else {
+                                    throw new InvalidDataEntryException("invalid image");
+                                }
+                            } else {
+                                throw new InvalidDataEntryException("invalid password");
+                            }
+                        } else {
+                            throw new InvalidDataEntryException("invalid username");
+                        }
+                    } else {
+                        throw new InvalidDataEntryException("invalid email");
+                    }
+                } else {
+                    throw new InvalidDataEntryException("invalid nic or passport number");
+                }
+            } else {
+                throw new InvalidDataEntryException("invalid address");
+            }
+        } else {
+            throw new InvalidDataEntryException("invalid name");
+        }
+    }
 }
