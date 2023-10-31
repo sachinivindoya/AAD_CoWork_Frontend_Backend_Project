725c985cf021110402f6eff597c4eb7c10f2a71f
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-31 13:32:47 +0530

    user/ admin front end implement update

---
+++ Frontend/pages/UserAdmin/js/userAdminCustome/ClientManageSub.js
@@ -0,0 +1,20 @@
+
+
+//-----Save New Client--------------
+const c_m_s_a_username = $('#c_m_s_a_username');
+const c_m_s_a_nameinitial = $('#c_m_s_a_nameinitial');
+const c_m_s_a_password = $('#c_m_s_a_password');
+const c_m_s_a_email = $('#c_m_s_a_email');
+const c_m_s_a_address = $('#c_m_s_a_address');
+const c_m_s_a_nic = $('#c_m_s_a_nic');
+const c_m_s_a_image = $('#c_m_s_a_image');
+const c_m_s_a_image_input = $('#c_m_s_a_image_input');
+const c_m_s_a_nic_front_image = $('#c_m_s_a_nic_front_image');
+const c_m_s_a_nic_rear_image = $('#c_m_s_a_nic_rear_image');
+const c_m_s_a_gender = $('#c_m_s_a_gender');
+const c_m_s_a_age = $('#c_m_s_a_age');
+const c_m_s_a_tell = $('#c_m_s_a_tell');
+const c_m_s_a_remarks = $('#c_m_s_a_remarks');
+const c_m_s_a_nic_front_image_input = $('#c_m_s_a_nic_front_image_input');
+const c_m_s_a_nic_rear_image_input = $('#c_m_s_a_nic_rear_image_input');
+const c_m_s_a_savebtn = $('#c_m_s_a_savebtn');
