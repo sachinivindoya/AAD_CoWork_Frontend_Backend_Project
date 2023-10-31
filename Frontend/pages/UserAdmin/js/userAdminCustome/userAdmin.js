3fa8c71bb8c92936eee13893c851d22f6c041e0f
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-10-31 10:32:09 +0530

    user/ admin front end implement update

--- Frontend/pages/UserAdmin/js/userAdminCustome/userAdmin.js
+++ Frontend/pages/UserAdmin/js/userAdminCustome/userAdmin.js
@@ -1,6 +1,47 @@
 
 //=================================================================================================================
+//navigation
 
+//---navigate with other containers-----
+const home_container = $("#home_container");
+const user_manage_container = $("#user_manage_container");
+const client_manage_container = $("#client_manage_container");
+const admin_manage_container = $("#admin_manage_container");
+const report_container = $("#report_container");
+
+
+function hideAllContainers(){
+    home_container.css('display','none');
+    user_manage_container.css('display','none');
+    client_manage_container.css('display','none');
+    admin_manage_container.css('display','none');
+    report_container.css('display','none');
+}
+
+$(document).ready(function(){
+    openHomeContainer();
+});
+
+function openHomeContainer(){
+    hideAllContainers();
+    home_container.css('display','block');
+}
+
+function openClientMngContainer(){
+    hideAllContainers();
+    client_manage_container.css('display','block');
+}
+
+function openAdminMngContainer(){
+    hideAllContainers();
+    admin_manage_container.css('display','block');
+}
+
+function openFinanceDataContainer(){
+    hideAllContainers();
+    report_container.css('display','block');
+}
+//==============================================================================================================
 //add user  form
 
 //field variable
