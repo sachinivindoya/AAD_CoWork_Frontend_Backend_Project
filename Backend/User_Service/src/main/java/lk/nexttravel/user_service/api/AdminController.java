1a14b67343bac9c331e23cabbd438eb06c421ef0
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 21:59:45 +0530

    user service Admin Controller

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/api/AdminController.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/api/AdminController.java
@@ -1,10 +1,16 @@
 package lk.nexttravel.user_service.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 21:57
  */
-
+@RestController
+@RequestMapping("/admin")
+@CrossOrigin("*")
 public class AdminController {
 }
