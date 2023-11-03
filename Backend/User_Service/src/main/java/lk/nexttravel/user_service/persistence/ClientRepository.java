d5107fd134998b41b3835b1f72efacfaac822c74
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:34:21 +0530

    implement persistence client repository user micro service

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/persistence/ClientRepository.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/persistence/ClientRepository.java
@@ -1,9 +1,18 @@
 package lk.nexttravel.user_service.persistence;
 
+import lk.nexttravel.user_service.entity.Client;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:33
  */
-public interface ClientRepository {
+@Repository
+public interface ClientRepository extends MongoRepository<Client, Long> {
+    Optional<Client> findClientById(String id);
+
 }
