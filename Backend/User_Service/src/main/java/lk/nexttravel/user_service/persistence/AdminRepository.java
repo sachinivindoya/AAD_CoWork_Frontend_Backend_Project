5138133ba8c93ab79cc22cc0668367ecd44b60b2
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:32:42 +0530

    implement persistence admin repository interface

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/persistence/AdminRepository.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/persistence/AdminRepository.java
@@ -1,9 +1,18 @@
 package lk.nexttravel.user_service.persistence;
 
+import lk.nexttravel.user_service.entity.Admin;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
- * Time    : 22:30
+ * Time    : 22:31
  */
-public class AdminRepository {
+@Repository
+public interface AdminRepository extends MongoRepository<Admin, Long> {
+    Optional<Admin> findAdminById(String id);
+
 }
