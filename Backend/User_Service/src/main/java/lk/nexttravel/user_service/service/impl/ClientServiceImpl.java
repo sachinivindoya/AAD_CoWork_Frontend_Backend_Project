8a28c6b33fb7b7f35aaba707329e00a2c3c713ee
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:56:19 +0530

    implement client service admin override method

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/service/impl/ClientServiceImpl.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/service/impl/ClientServiceImpl.java
@@ -1,9 +1,11 @@
 package lk.nexttravel.user_service.service.impl;
 
 import lk.nexttravel.user_service.dto.ReqNewClientSaveDTO;
+import lk.nexttravel.user_service.entity.Client;
 import lk.nexttravel.user_service.persistence.ClientRepository;
 import lk.nexttravel.user_service.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 /**
@@ -24,16 +26,81 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public ResponseEntity<String> SaveNewClient_Prepare(ReqNewClientSaveDTO reqNewClientSaveDTO) {
-        return null;
+        //check authentication
+        try {
+            if (apiGatewayJwtAccessTokenServiceBackend.isTokenValid(reqNewClientSaveDTO.getToken())) {  //check gateway token
+                //save into database
+                Client client = clientRepository.save(
+                        Client.builder()
+                                .id(reqNewClientSaveDTO.getId())
+                                .name_with_initial(reqNewClientSaveDTO.getName_with_initial())
+                                .address(reqNewClientSaveDTO.getAddress())
+                                .profile_image(reqNewClientSaveDTO.getProfile_image())
+                                .nic_or_passport(reqNewClientSaveDTO.getNic_or_passport())
+                                .transaction_state(RespondCodes.PENDING)
+                                .build()
+                );
+
+                return new ResponseEntity<>(RespondCodes.Respond_DATA_SAVED, HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(RespondCodes.Respond_NOT_AUTHORISED, HttpStatus.UNAUTHORIZED);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(RespondCodes.Respond_SERVERSIDE_INTERNAL_FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+}
 
     @Override
     public ResponseEntity<String> SaveNewClient_Commit(ReqNewClientSaveDTO reqNewClientSaveDTO) {
-        return null;
+        //check authentication
+        try {
+            if (apiGatewayJwtAccessTokenServiceBackend.isTokenValid(reqNewClientSaveDTO.getToken())) {  //check gateway token
+                //save into database
+                clientRepository.save(
+                        Client.builder()
+                                .id(reqNewClientSaveDTO.getId())
+                                .name_with_initial(reqNewClientSaveDTO.getName_with_initial())
+                                .address(reqNewClientSaveDTO.getAddress())
+                                .profile_image(reqNewClientSaveDTO.getProfile_image())
+                                .nic_or_passport(reqNewClientSaveDTO.getNic_or_passport())
+                                .transaction_state(RespondCodes.COMMITED)
+                                .build()
+                );
+
+                return new ResponseEntity<>(RespondCodes.Respond_DATA_SAVED, HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(RespondCodes.Respond_NOT_AUTHORISED, HttpStatus.UNAUTHORIZED);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(RespondCodes.Respond_SERVERSIDE_INTERNAL_FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @Override
     public ResponseEntity<String> SaveNewClient_Abrot(ReqNewClientSaveDTO reqNewClientSaveDTO) {
-        return null;
+        try {
+            if (apiGatewayJwtAccessTokenServiceBackend.isTokenValid(reqNewClientSaveDTO.getToken())) {  //check gateway token
+                //delete
+                clientRepository.delete(
+                        Client.builder()
+                                .id(reqNewClientSaveDTO.getId())
+                                .name_with_initial(reqNewClientSaveDTO.getName_with_initial())
+                                .address(reqNewClientSaveDTO.getAddress())
+                                .profile_image(reqNewClientSaveDTO.getProfile_image())
+                                .nic_or_passport(reqNewClientSaveDTO.getNic_or_passport())
+                                .build()
+                );
+
+                return new ResponseEntity<>(RespondCodes.Respond_DATA_DELETED, HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(RespondCodes.Respond_NOT_AUTHORISED, HttpStatus.UNAUTHORIZED);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(RespondCodes.Respond_SERVERSIDE_INTERNAL_FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     }
 }
