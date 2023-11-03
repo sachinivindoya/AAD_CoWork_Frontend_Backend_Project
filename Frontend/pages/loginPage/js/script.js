d3d50584fda06cd83bbec45bb339bd75cd2f3ad4
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 06:41:11 +0530

    login js

--- Frontend/pages/loginPage/js/script.js
+++ Frontend/pages/loginPage/js/script.js
@@ -1,4 +1,4 @@
-///////////////////--------------component variables
+//////////////////////--------------component variables
 
 const login_container = $('#login_container');
 
@@ -15,8 +15,7 @@ const intrologin_pg_alertModelDone_content = $('#intro-login-pg-alert-model-cont
 const pswd_recovery_login_username_txtfld = $('#pswd_recovery_login_username');
 const pswd_recovery_login_username_container = $('#forgot_pswd_get_username_container');
 
-
-//------------------ login ----------------
+////////---------- login operation
 //fields variables
 let loginUsername =  $('#login_Username');
 let loginPassword =  $('#login_Password');
@@ -32,4 +31,61 @@ function isUsernameCheckedRegexLogin(username) {
 function isSignUpPasswordCheckedRegexLogin(password) {
     const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     return regex.test(password);
+}
+
+//check regax action signup_password
+loginUsername.on('keyup', function () {
+    const tinvalidFeedback = loginUsername.siblings('.invalid-feedback');
+    const tvalidFeedback = loginUsername.siblings('.valid-feedback');
+    if (isUsernameCheckedRegexLogin(loginUsername.val())) {
+        loginUsername.removeClass('is-invalid');
+        loginUsername.addClass('is-valid');
+        tvalidFeedback.show();
+        tinvalidFeedback.hide();
+
+        logintxtfld1=true;
+
+        readyToEnableLoginBtn();//trigger to enable
+    } else {
+        loginUsername.removeClass('is-valid');
+        loginUsername.addClass('is-invalid');
+        tinvalidFeedback.show();
+        tvalidFeedback.hide();
+
+        logintxtfld1=false;
+
+        readyToEnableLoginBtn();//trigger to enable
+    }
+});
+
+//check regax action signup_password
+loginPassword.on('keyup', function () {
+    const tinvalidFeedback = loginPassword.siblings('.invalid-feedback');
+    const tvalidFeedback = loginPassword.siblings('.valid-feedback');
+    if (isSignUpPasswordCheckedRegexLogin(loginPassword.val())) {
+        loginPassword.removeClass('is-invalid');
+        loginPassword.addClass('is-valid');
+        tvalidFeedback.show();
+        tinvalidFeedback.hide();
+
+        logintxtfld2=true;
+
+        readyToEnableLoginBtn();//trigger to enable
+    } else {
+        loginPassword.removeClass('is-valid');
+        loginPassword.addClass('is-invalid');
+        tinvalidFeedback.show();
+        tvalidFeedback.hide();
+
+        logintxtfld2=false;
+
+        readyToEnableLoginBtn();//trigger to enable
+    }
+});
+
+function readyToEnableLoginBtn() {
+    if (logintxtfld1 === true && logintxtfld2 === true ) {
+        // All conditions are true, enable the signup button
+        user_login_btn.prop("disabled", false);
+    }
 }
