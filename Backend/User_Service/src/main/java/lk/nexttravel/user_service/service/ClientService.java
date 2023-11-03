b4ddf1119b89569af51fa2ec42c5d6d852e2e77a
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:40:05 +0530

    implement service layer client service interface

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/service/ClientService.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/service/ClientService.java
@@ -1,9 +1,17 @@
 package lk.nexttravel.user_service.service;
 
+import lk.nexttravel.user_service.dto.ReqNewClientSaveDTO;
+import org.springframework.http.ResponseEntity;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:39
  */
 public interface ClientService {
+    ResponseEntity<String> SaveNewClient_Prepare(ReqNewClientSaveDTO reqNewClientSaveDTO);
+
+    ResponseEntity<String> SaveNewClient_Commit(ReqNewClientSaveDTO reqNewClientSaveDTO);
+
+    ResponseEntity<String> SaveNewClient_Abrot(ReqNewClientSaveDTO reqNewClientSaveDTO);
 }
