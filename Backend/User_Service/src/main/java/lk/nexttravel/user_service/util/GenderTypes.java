1b3f8858e89a7ad7a8a1d568c6eda0136f1fd462
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 23:02:13 +0530

    implement util package gender types

---
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/util/GenderTypes.java
@@ -0,0 +1,11 @@
+package lk.nexttravel.user_service.util;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 03/11/2023
+ * Time    : 23:01
+ */
+public enum GenderTypes {
+    MALE,
+    FEMALE
+}
