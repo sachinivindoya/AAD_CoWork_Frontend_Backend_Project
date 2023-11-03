a691ceb568e66e68338de5407a4742af900e7f2b
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:13:57 +0530

    user service dto

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/dto/ReqNewClientSaveDTO.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/dto/ReqNewClientSaveDTO.java
@@ -1,9 +1,20 @@
 package lk.nexttravel.user_service.dto;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:13
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
+@Component
 public class ReqNewClientSaveDTO {
+
 }
