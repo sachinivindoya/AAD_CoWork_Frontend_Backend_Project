74de71f7c3d9bae91012f0878c814c059d1fb5cc
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:38:13 +0530

    implement service layer in user micro service

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/service/AdminService.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/service/AdminService.java
@@ -1,6 +1,9 @@
 package lk.nexttravel.user_service.service;
 
 import lk.nexttravel.user_service.dto.ReqNewClientSaveDTO;
+import lk.nexttravel.user_service.dto.ReqProfileDataAdminsDTO;
+import lk.nexttravel.user_service.dto.ReqUpdateGuideAdminDTO;
+import lk.nexttravel.user_service.entity.Admin;
 import org.springframework.http.ResponseEntity;
 
 /**
@@ -16,4 +19,13 @@ public interface AdminService {
 
     ResponseEntity<String> SaveNewClient_Abrot(ReqNewClientSaveDTO reqNewClientSaveDTO);
 
+    //get user admin profile image
+    ResponseEntity<ReqProfileDataAdminsDTO> userAdminGetProfileData(String id, String token);
+
+    //get admin by id
+    ResponseEntity<Admin> getAllAdminsSataNySearch(String id , String token);
+
+    ResponseEntity<String> SaveUpdatedAdmin_Prepare(ReqUpdateGuideAdminDTO reqUpdateGuideAdminDTO);
+
+    ResponseEntity<String> SaveUpdatedAdmin_Commit(ReqUpdateGuideAdminDTO reqUpdateGuideAdminDTO);
 }
