ef8d34f837406f70ca125d21a600064b0fb948dc
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:22:53 +0530

    implement respond dto in user service

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/dto/RespondDTO.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/dto/RespondDTO.java
@@ -1,9 +1,23 @@
 package lk.nexttravel.user_service.dto;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:21
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
+@Component
 public class RespondDTO {
+    private String rspd_code;
+    private String repd_msg;
+    private Object token;
+    private Object data;
 }
