ef4d28491ede37946b74ef28361fd818c2a51c88
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:17:32 +0530

    data admin dto

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/dto/ReqProfileDataAdminsDTO.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/dto/ReqProfileDataAdminsDTO.java
@@ -1,9 +1,29 @@
 package lk.nexttravel.user_service.dto;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:15
  */
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
+@Component
 public class ReqProfileDataAdminsDTO {
+
+    private String name_with_initial;
+
+    private String nic_or_passport;
+
+    private String address;
+
+    private String profile_image;
 }
