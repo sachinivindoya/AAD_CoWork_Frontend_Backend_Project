e97f6e495abca373b782d687bc643181ee88b435
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:21:05 +0530

    guide admin dto

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/dto/ReqUpdateGuideAdminDTO.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/dto/ReqUpdateGuideAdminDTO.java
@@ -1,9 +1,19 @@
 package lk.nexttravel.user_service.dto;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:20
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
+@Component
 public class ReqUpdateGuideAdminDTO {
 }
