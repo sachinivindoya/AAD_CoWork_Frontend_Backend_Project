75930ba82dfbf2954aec009276d84a768dc79865
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 06:39:17 +0530

    login page implement

---
+++ Frontend/pages/loginPage/js/script.js
@@ -0,0 +1,16 @@
+///////////////////--------------component variables
+
+const login_container = $('#login_container');
+
+const intrologin_pg_loadingModel = $('#intro-login-pg-loading-model');
+
+const intrologin_pg_alertModelError = $('#intro-login-pg-alert-model-error');
+const intrologin_pg_alertModelError_title =  $('#intro-login-pg-alert-model-title-error');
+const intrologin_pg_alertModelError_content = $('#intro-login-pg-alert-model-content-error');
+
+const intrologin_pg_alertModelDone = $('#intro-login-pg-alert-model-done');
+const intrologin_pg_alertModelDone_title =  $('#intro-login-pg-alert-model-title-done');
+const intrologin_pg_alertModelDone_content = $('#intro-login-pg-alert-model-content-done');
+
+const pswd_recovery_login_username_txtfld = $('#pswd_recovery_login_username');
+const pswd_recovery_login_username_container = $('#forgot_pswd_get_username_container');
