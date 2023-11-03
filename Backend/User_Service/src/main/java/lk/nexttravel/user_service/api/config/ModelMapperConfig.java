a4f277862a0c4223b31c8ab8c0d3486f8355ffde
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:10:24 +0530

    user service model mapper config

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/api/config/ModelMapperConfig.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/api/config/ModelMapperConfig.java
@@ -1,9 +1,17 @@
 package lk.nexttravel.user_service.api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:08
  */
+@Configuration
 public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
