a742596a514d568f8862139f7394123c049ef2a3
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:29:57 +0530

    implement entity cliente in user service

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/entity/Client.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/entity/Client.java
@@ -1,9 +1,35 @@
 package lk.nexttravel.user_service.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:28
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Builder
+
+@Document(collection = "Client")
 public class Client {
+    @Id
+    private String id;
+    private String name_with_initial;
+    private String address;
+    private String profile_image;
+    private String nic_or_passport;
+    private String nic_or_passport_front_view;
+    private String nic_or_passport_rear_view;
+    private GenderTypes gender;
+    private long Tell;
+    private String Remarks;
+    private int age;
+
+    //for 2PC transaction
+    private String transaction_state;
 }
