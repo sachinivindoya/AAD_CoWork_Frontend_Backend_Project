33ac6240ba8b6695e84fbaa5d51a59be00f77ee1
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:26:56 +0530

    implement entity package in user service

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/entity/Admin.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/entity/Admin.java
@@ -1,9 +1,30 @@
 package lk.nexttravel.user_service.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:25
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Builder
+
+@Document(collection = "Admin")
 public class Admin {
+    @Id
+    private String id;
+    private String signup_name_with_initial;
+    private String nic_or_passport;
+    private String address;
+    private long salary;
+    private String profile_image;
+
+    //for 2PC transaction
+    private String transaction_state;
 }
