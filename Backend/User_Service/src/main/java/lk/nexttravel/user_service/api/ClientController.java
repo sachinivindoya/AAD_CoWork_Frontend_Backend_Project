6cd168a6280b0a0f3b9ad58530d4c6214d4ae91d
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-03 22:06:04 +0530

    client controller

--- Backend/User_Service/src/main/java/lk/nexttravel/user_service/api/ClientController.java
+++ Backend/User_Service/src/main/java/lk/nexttravel/user_service/api/ClientController.java
@@ -1,9 +1,16 @@
 package lk.nexttravel.user_service.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 03/11/2023
  * Time    : 22:05
  */
+@RestController
+@RequestMapping("/client")
+@CrossOrigin("*")
 public class ClientController {
 }
