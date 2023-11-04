bf5f528a31b93ba4de1c44136e547bab929e2959
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-04 07:25:55 +0530

    api gatway advicer respondExceptionHandler

---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/advice/RespondExceptionHandler.java
@@ -0,0 +1,12 @@
+package lk.nexttravel.apigateway.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 04/11/2023
+ * Time    : 07:25
+ */
+@ControllerAdvice
+public class RespondExceptionHandler {
+}
