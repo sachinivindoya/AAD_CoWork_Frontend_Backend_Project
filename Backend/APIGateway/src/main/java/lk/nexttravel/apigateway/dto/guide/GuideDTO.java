846d555fd77ca16d4fbb4a043183a7f746d73205
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-04 08:07:36 +0530

    api gatway guide dto

---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/dto/guide/GuideDTO.java
@@ -0,0 +1,47 @@
+package lk.nexttravel.apigateway.dto.guide;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 04/11/2023
+ * Time    : 08:05
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
+@Component
+public class GuideDTO {
+    private String id;
+
+    private String name;
+
+    private String remarks;
+
+    private int experience;
+
+    private String nic;
+
+    private String nic_front_view;
+
+    private String nic_rear_view;
+
+    private String tell;
+
+    private String gender;
+
+    private String dob;
+
+    private String image;
+
+    private String address;
+
+    private int perday_fee;
+
+    //for 2PC transaction
+    private String transaction_state;
+}
---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/dto/user/AdminDTO.java
@@ -0,0 +1,9 @@
+package lk.nexttravel.apigateway.dto.user;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 04/11/2023
+ * Time    : 08:07
+ */
+public class AdminDTO {
+}
