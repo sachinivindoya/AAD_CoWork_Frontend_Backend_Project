28cbc1b86398091b5e24d6774f9132456028714c
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-04 10:36:08 +0530

    guide microservice guide api

--- Backend/Guide_Service/src/main/java/lk/nexttravel/guide_service/api/GuideController.java
+++ Backend/Guide_Service/src/main/java/lk/nexttravel/guide_service/api/GuideController.java
@@ -1,9 +1,52 @@
 package lk.nexttravel.guide_service.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 04/11/2023
  * Time    : 10:35
  */
+
+
+@RestController
+@RequestMapping("/guide")
+@CrossOrigin("*")
 public class GuideController {
+
+    @Autowired
+    GuideService guideService;
+
+    //----------Save New Guide ------
+    @PostMapping(value = "/save_new_guide")
+    public ResponseEntity<String> SaveNewGuide(@RequestBody ReqNewGuideSaveDTO reqNewGuideSaveDTO){
+        return guideService.SaveNewGuide(reqNewGuideSaveDTO);
+    }
+
+    //----------update Guide ------
+    @PutMapping(value = "/save_new_guide")
+    public ResponseEntity<String> updateGuidee(@RequestBody ReqNewGuideSaveDTO reqNewGuideSaveDTO){
+        return guideService.updateGuide(reqNewGuideSaveDTO);
+    }
+
+    //search all guides
+    @GetMapping(value = "/getall-guides")
+    public ResponseEntity<ArrayList<Guide>> getAllGuides(
+            @RequestParam String token
+    ){
+        return guideService.getAllGuides(token);
+    }
+
+    //sdelete guide
+    @DeleteMapping(value = "/delete-guide")
+    public ResponseEntity<String> deleteGuide(
+            @RequestParam String id,
+            @RequestParam String token
+    ){
+        return guideService.deleteGuide(id, token);
+    }
 }
