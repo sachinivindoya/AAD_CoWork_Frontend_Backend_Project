c9a8aa3c9b695b959ca5f1e725a672416b806e9a
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-04 07:54:47 +0530

    api gatway config security config

---
+++ Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package lk.nexttravel.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @author : R.M.Sachini Vinodya
+ * Date    : 04/11/2023
+ * Time    : 07:53
+ */
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        UserDetails users = User.builder()
+                .username("user")
+                .password(encoder.encode("user"))
+                .roles("USER")
+                .build();
+        return  new MapReactiveUserDetailsService((users));
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity https){
+        return https
+                .authorizeExchange(auth -> {
+//                    auth.pathMatchers("/http://localhost:63342/**").permitAll();  //mekata danone client url eka
+                    auth.anyExchange().permitAll(); //okotm permit krl thinne error nisa
+                })
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
+
+
+}
