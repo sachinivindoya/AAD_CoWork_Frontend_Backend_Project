0d88450c25cee9a95d2296e6cba8f2916c1a1b3b
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-04 10:33:04 +0530

    guide microservice recode

--- Backend/Guide_Service/src/main/java/lk/nexttravel/guide_service/advice/util/NotfoundException.java
+++ Backend/Guide_Service/src/main/java/lk/nexttravel/guide_service/advice/util/NotfoundException.java
@@ -3,10 +3,10 @@ package lk.nexttravel.guide_service.advice.util;
 /**
  * @author : R.M.Sachini Vinodya
  * Date    : 04/11/2023
- * Time    : 10:25
+ * Time    : 10:32
  */
-public class InternalServerException extends RuntimeException{
-    public InternalServerException(String message){
+public class NotfoundException extends RuntimeException{
+    public NotfoundException(String message){
         super(message);
     }
 }
