922e2b39a72d1e29c91f2d2df84e9f49a7c7b933
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-04 10:34:05 +0530

    guide microservice exception handling

--- Backend/Guide_Service/src/main/java/lk/nexttravel/guide_service/advice/RespondExceptionHandler.java
+++ Backend/Guide_Service/src/main/java/lk/nexttravel/guide_service/advice/RespondExceptionHandler.java
@@ -1,6 +1,11 @@
 package lk.nexttravel.guide_service.advice;
 
+import lk.nexttravel.guide_service.advice.util.InternalServerException;
+import lk.nexttravel.guide_service.advice.util.NotfoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * @author : R.M.Sachini Vinodya
@@ -9,4 +14,28 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @ControllerAdvice
 public class RespondExceptionHandler {
+
+
+    @ExceptionHandler(NotfoundException.class)
+    protected ResponseEntity<RespondDTO> exception(NotfoundException notfoundException) {
+        return new ResponseEntity<RespondDTO>(
+                (RespondDTO.builder()
+                        .rspd_code(RespondCodes.Respond_NO_DATA_FOUND)
+                        .rspd_code(notfoundException.getMessage())
+                        .token(null)
+                        .data(null)
+                        .build()
+                ),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(InternalServerException.class)
+    protected ResponseEntity<RespondDTO> exception(InternalServerException exception) {
+        System.out.println("InternalServerException"+exception.getMessage());
+        return new ResponseEntity<>(
+                new RespondDTO(RespondCodes.Respond_SERVERSIDE_INTERNAL_FAIL,exception.getMessage(), null,null)
+                , HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
