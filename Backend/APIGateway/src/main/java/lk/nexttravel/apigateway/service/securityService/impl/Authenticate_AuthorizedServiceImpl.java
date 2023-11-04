2c70e6e027e693920b13295063772be5f22ce035
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-04 07:24:43 +0530

    api gatway recode

--- Backend/APIGateway/src/main/java/lk/nexttravel/apigateway/service/securityService/impl/Authenticate_AuthorizedServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package lk.nexttravel.apigateway.service.securityService.impl;
-
-/**
- * @author : R.M.Sachini Vinodya
- * Date    : 31/10/2023
- * Time    : 01:00
- */
-public class Authenticate_AuthorizedServiceImpl {
-}
